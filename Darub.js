a08b5e7c073e2f9e21e27ed885d82cadaf14d311
software <software@SOFTWAREs-iMac.local>
2015-10-18 12:36:27 -0400

    remove redundant files and move images to img/

--- Darub.js
+++ Darub.js
@@ -1,3 +1,8 @@
+/ DO NOT ALTER THIS CONTENT
+// IT DIRECTS THE ENTRY MODULES AND THE CONTENT WITHIN 
+// THEM HOW TO ALLIGN THEMSELVES UPON SWITCHING BETWEEN
+// EACH DIRECTION THRESHOLD
+
 $(function(){
     $(".element").click(function() {  
         var iteration=$(this).data('iteration')||1
@@ -30,50 +35,62 @@ $(function(){
     })
 })
 
-// Div orientation
+// ONLY EDITABLE CONTENT BELOW, 
+// ADD THE NAME OF NEW ELEMENTS HERE IF THEY HAVE AN 
+// INITIAL DIRECETION CLASS OF EAST OR WEST
 
 var $divs = $('#one, #two, #four' )
+
+//DO NOT ALTER CONTENT BELOW
+
 var midX = $('#cumpass').offset().left;
 
 $divs.draggable({
     drag: function () {
         var cumpass = $(this), x = $(this).offset().left;
-       	var img = $(this).children(img), imgX = img[1].offsetWidth, imgMargin = 0 - imgX; theX = x + imgX;
-		//console.log(imgX,"+",midX,"=",theX, imgMargin);	
-
+        var img = $(this).children(img), imgX = img[1].offsetWidth, imgMargin = 0 - imgX; theX = x + imgX;
+        
         if(theX > midX){
-			$(this).addClass("west"); 
-        	$(this).removeClass("east");  
+            $(this).addClass("west"); 
+            $(this).removeClass("east");  
+        
         }
 
         if(theX < midX){
-        	$(this).addClass("east"); 
-        	$(this).removeClass("west"); 
+            $(this).addClass("east"); 
+            $(this).removeClass("west"); 
         }
 
     }
 });
 
+// ONLY EDITABLE CONTENT BELOW, 
+// ADD THE NAME OF NEW ELEMENTS HERE IF THEY HAVE AN 
+// INITIAL DIRECETION CLASS OF NORTH OR SOUTH
+
 var $divs = $('#three')
+
+//DO NOT ALTER CONTENT BELOW
+
 var midY = $('#cumpass').offset().top;
 
 $divs.draggable({
     drag: function () {
         var cumpass = $(this), y = $(this).offset().top; 
-        var img = $(this).children(img);		 
-	    var text = $(this).children(".text");
-	    var h1 = $(this).children(h1);
-		 
+        var img = $(this).children(img);         
+        var text = $(this).children(".text");
+        var h1 = $(this).children(h1);
+         
         if(y < midY){
-        	$(this).addClass("north"); 
-        	$(this).removeClass("south"); 
-        	$(img).insertBefore(text);
+            $(this).addClass("north"); 
+            $(this).removeClass("south"); 
+            $(img).insertBefore(text);
         }
 
         if(y > midY){
-        	$(this).addClass("south"); 
-        	$(this).removeClass("north");
-        	$(img).insertAfter(text);
+            $(this).addClass("south"); 
+            $(this).removeClass("north");
+            $(img).insertAfter(text);
         }
 
     }
